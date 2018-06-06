886e20fb87547b4938832be31c9a3f8b7775a210
hirarinNakalab <hirakawa.rin151@mail.kyutech.jp>
2018-06-06 14:28:46 +0900

    ICISIP

--- plot_ROC_result.py
+++ plot_ROC_result.py
@@ -1,11 +1,13 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from gensim import models
 from sklearn import metrics
 from scipy import spatial
 
 
-LOAD_MODEL = 'doc2vec_houhan(saigen).model'
+LOAD_MODEL = 'doc2vec_houhan(saigen-size300).model'
 HOUHAN_PATH = './text/HH/'
 
 # exec(open('./parseValidFiles.py').read())
@@ -35,14 +37,15 @@ for root, dirs, files in os.walk(HOUHAN_PATH):
 y = np.array(labels)
 scores = np.array(values)
 
-fpr, tpr, thresholds = metrics.roc_curve(y, scores, pos_label=2)
+fpr, tpr, thresholds = metrics.roc_curve(y, scores, pos_label=2, drop_intermediate=False)
+print(thresholds)
 auc = metrics.auc(fpr, tpr)
 
-plt.plot(fpr, tpr, label='ROC curve (area = %.2f)' %auc)
+plt.plot(fpr, tpr, label='cosine similarity of PV-DM (area = %.2f)' %auc)
 plt.legend()
-plt.title('ROC curve')
+plt.title('Sales Visit - Receiver Operating Characteristic')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.grid(True)
 plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Luck', alpha=.8)
-plt.show()
+plt.savefig('roc.png')
